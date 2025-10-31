f36aae13ed0808ca00a31ace55a8c06be5154336
qiu23-person <2140903946@qq.com>
2025-10-31 16:27:58 +0800

    hw0b: Completed Enhancedfor.java

--- hw0b/src/Enhancedfor.java
+++ hw0b/src/Enhancedfor.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Enhancedfor {
+    public static void main(String args[]){
+          String[] a = {"cat", "dog", "laser horse", "ketchup", "horse", "horbse"};
+        // commpm edition
+        //        for (int i = 0; i < a.length; i += 1) {
+        //            for (int j = 0; j < 3; j += 1) {
+        //                System.out.println(a[i]);
+        //                if (a[i].contains("horse")) {
+        //                    break;
+        //                }
+        //            }
+        //        }
+        //enhanced for loop
+//        for(String S: a){
+//            for(int j=0;j<3;j++){
+//                System.out.println(S);
+//                if(S.contains("horse")) break;
+//            }
+//        }
+    }
 }
