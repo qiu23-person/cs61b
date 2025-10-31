348f64aaad49628cc756d6c502fa8f9ff506745f
qiu23-person <2140903946@qq.com>
2025-10-31 16:19:41 +0800

    hw0b: Completed ClassNameHere.java

--- hw0b/src/ClassNameHere.java
+++ hw0b/src/ClassNameHere.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class ClassNameHere {
+    //返回数组中的最大值，所有元素大小>=0
+    public static int max(int[] m){
+        int temp=m[0];
+        //在java中获取数组长度的变量是length
+        for(int i=0;i<m.length;i++){
+            if(temp<m[i]){
+                temp=m[i];
+            }
+        }
+
+        return temp;
+    }
+    public static void main(String arg[]){
+        int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6,77};
+        //打印最大值
+        int max=max(numbers);
+        System.out.println("该数组中的最大值是:"+max);
+    }
+
 }
