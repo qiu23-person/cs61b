59dc341a4d2d0ba3f21e84092fc488e5fc9780d5
qiu23-person <2140903946@qq.com>
2025-11-09 14:47:01 +0800

    hw0b : Completed MapExercises.java and otherExercises.java

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Dessert {
+    private int flavor;
+    private int price;
+    private static int numDesserts=0;
+    Dessert(int flavor,int price){
+        this.flavor=flavor;
+        this.price=price;
+        numDesserts++;
+    }
+    public void printDessert( ){
+
+        System.out.println( this.flavor+" "+this.price+" "+  numDesserts);
+    }
+    public static void main(String[] args){
+        System.out.println("I love dessert!");
+    }
 }
